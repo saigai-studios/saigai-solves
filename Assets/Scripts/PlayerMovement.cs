eb74645eb9d1103d10b2f092c88288ee45272281
Thomas Le <thomas.le@ufl.edu>
2024-06-18 22:06:21 +0900

    Moved player movement to Rust

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,37 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Saigai.Studios;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Transform[] markers = new Transform[3];
-    public float[,] marker_xz = new float[3,2];
-    float[] old_pos = new float[2];
-    float[] new_pos = new float[2];
-    static int speed = 100;
-    int curr_mark = 0;
-    int counter = 0;
+    const float static_y = 10;
     
     // Start is called before the first frame update
     void Start()
     {
+        Vec2[] marker_xz = new Vec2[3];
+        
         // Get positions
-        markers[0] = GameObject.Find("Minigame Marker 1").transform;
-        marker_xz[0,0] = markers[0].position.x;
-        marker_xz[0,1] = markers[0].position.z;
-        markers[1] = GameObject.Find("Minigame Marker 2").transform;
-        marker_xz[1,0] = markers[1].position.x;
-        marker_xz[1,1] = markers[1].position.z;
-        markers[2] = GameObject.Find("Minigame Marker 3").transform;
-        marker_xz[2,0] = markers[2].position.x;
-        marker_xz[2,1] = markers[2].position.z;
-
-        // Start at marker 0
-        transform.position = markers[0].position;
-
-        // Reset counter
-        // Setting it to the largest possible value will end 
-        counter = speed;
+        for (int i = 0; i < 3; ++i){
+            string name = "Minigame Marker " + (i+1).ToString();
+            Debug.Log(name);
+            marker_xz[i].x = GameObject.Find(name).transform.position.x;
+            marker_xz[i].y = GameObject.Find(name).transform.position.z;
+            Interop.init_marker(i, marker_xz[i]);
+        }
     }
 
     // Update is called once per frame
@@ -39,57 +27,15 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown("left"))
         {
-            curr_mark -= 1;
-
-            if (curr_mark < 0)
-            {
-                curr_mark = 2;
-            }
-
-            counter = 0;
-            old_pos[0] = transform.position.x;
-            old_pos[1] = transform.position.z;
-            new_pos[0] = marker_xz[curr_mark,0];
-            new_pos[1] = marker_xz[curr_mark,1];
+            Interop.update_pos(true);
         }
         else if (Input.GetKeyDown("right"))
         {
-            curr_mark += 1;
-
-            if (curr_mark > 2)
-            {
-                curr_mark = 0;
-            }
-
-            counter = 0;
-            old_pos[0] = transform.position.x;
-            old_pos[1] = transform.position.z;
-            new_pos[0] = marker_xz[curr_mark, 0];
-            new_pos[1] = marker_xz[curr_mark, 1];
+            Interop.update_pos(false);
         }
 
-        //transform.position = markers[curr_mark].position;
-        if (counter <= speed)
-        {
-            float[] new_xz = move_lerp(counter, speed, old_pos, new_pos);
-            transform.position = new Vector3(new_xz[0], transform.position.y, new_xz[1]);
-            counter += 1;
-        }
-    }
-
-    float[] move_fake(float[,] states, int pos)
-    {
-        float[] test = new float[2];
-        test[0] = states[pos, 0];
-        test[1] = states[pos,1];
-        return test;
-    }
-
-    float[] move_lerp(int curr_time, int tot_time, float[] src, float[] dest)
-    {
-        float[] int_pos = new float[2];
-        int_pos[0] = Mathf.Lerp(src[0], dest[0], (float)curr_time / tot_time);
-        int_pos[1] = Mathf.Lerp(src[1], dest[1], (float)curr_time / tot_time);
-        return int_pos;
+        // Update animation
+        Vec2 new_pos = Interop.update_anim();
+        transform.position = new Vector3(new_pos.x, static_y, new_pos.y);
     }
 }
--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -2,9 +2,87 @@ use interoptopus::{ffi_function, ffi_type, function, Inventory, InventoryBuilder
 
 #[ffi_type]
 #[repr(C)]
+#[derive(Copy,Clone)]
 pub struct Vec2 {
     pub x: f32,
     pub y: f32,
+}
+
+const fn new() -> Vec2{
+    Vec2 { x: 0.0, y: 0.0 }
+}
+
+#[ffi_type]
+#[repr(C)]
+pub struct Player {
+    pub curr: Vec2,
+    pub old: Vec2,
+    pub dest: Vec2,
+    pub curr_mark: i32,
+    pub anim_count: i32,
+}
+
+// Anim constants
+const speed: i32 = 100;
+
+// Player variables
+static mut marker_pos: [Vec2; 3] = [new(); 3];
+static mut plr: Player = 
+Player{
+    curr: new(),
+    old: new(),
+    dest: new(),
+    curr_mark: 0,
+    anim_count: speed,
+};
+
+#[ffi_function]
+#[no_mangle]
+pub unsafe extern "C" fn init_marker(ind: i32, pos: Vec2){
+    marker_pos[ind as usize] = pos;
+}
+
+#[ffi_function]
+#[no_mangle]
+pub unsafe extern "C" fn update_pos(opt: bool) {
+    // Decrement and wrap
+    if opt{ // Left
+        plr.curr_mark -= 1;
+
+        if plr.curr_mark < 0{
+            plr.curr_mark = 2;
+        }
+    }
+    else{ // Right
+        plr.curr_mark += 1;
+
+        if plr.curr_mark > 2{
+            plr.curr_mark = 0;
+        }
+    }
+
+    // Reset animation counter
+    plr.anim_count = 0;
+
+    // Set starting position
+    plr.old = plr.curr;
+
+    // Set ending position
+    plr.dest = marker_pos[plr.curr_mark as usize];
+}
+
+#[ffi_function]
+#[no_mangle]
+pub unsafe extern "C" fn update_anim() -> Vec2{
+    if plr.anim_count <= speed{
+        plr.curr = move_lerp_rust(plr.anim_count, plr.old, plr.dest);
+        plr.anim_count += 1;
+    }
+    else {
+        plr.curr = marker_pos[plr.curr_mark as usize];
+    }
+
+    return plr.curr;
 }
 
 #[ffi_function]
@@ -22,11 +100,25 @@ pub extern "C" fn add_two_nums(x: i32, y: i32) -> i32 {
     result
 }
 
+pub fn move_lerp_rust(curr_time: i32, src: Vec2, dest: Vec2) -> Vec2 {
+    Vec2{
+        x: f_lerp(src.x, dest.x, (curr_time as f32) / (speed as f32)),
+        y: f_lerp(src.y, dest.y, (curr_time as f32) / (speed as f32))
+    }
+}
+
+pub fn f_lerp(src: f32, dest: f32, scale: f32) -> f32 {
+    return src + ((dest - src) * scale);
+}
+
 pub fn ffi_inventory() -> Inventory {
     {
         InventoryBuilder::new()
             .register(function!(my_function))
             .register(function!(add_two_nums))
+            .register(function!(update_anim))
+            .register(function!(init_marker))
+            .register(function!(update_pos))
             .inventory()
     }
 }
