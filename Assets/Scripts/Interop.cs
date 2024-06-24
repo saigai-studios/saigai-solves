3d9205f7f5e30e6218a35a7660ca0ae73b2e7b0d
Joseph Benigno <jbenigno@ufl.edu>
2024-06-24 16:18:02 +0900

    Localization + tables setup

--- Assets/Scripts/Interop.cs
+++ Assets/Scripts/Interop.cs
@@ -16,7 +16,7 @@ namespace Saigai.Studios
 {
     public static partial class Interop
     {
-        public const string NativeLib = "saigai";
+        public const string NativeLib = "saigai.caf4a839";
 
         static Interop()
         {
