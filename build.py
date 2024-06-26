a1009785eb5f1f3510f61a3c76435b8270ed08a4
Thomas Le <thomas.le@ufl.edu>
2024-06-26 16:50:25 +0900

    Added build script, fixed build.py

--- build.py
+++ build.py
@@ -9,9 +9,18 @@ import subprocess, sys
 UNITY_PATH = sys.argv[1]
 TARGET = sys.argv[2]
 
+if TARGET != "win64" and TARGET != "osxuniversal" and TARGET != "linux64":
+    print("Error: Invalid build target (must be \"win64\", \"osxuniversal\", or \"linux64\")")
+    quit()
+
+build_path = "Builds/" + TARGET
+
+print("***** BUILDING RUST DLL *****")
 child = subprocess.Popen(['cargo', 'run', '--release'], cwd='Rust')
 child.wait()
+print("***** STORING RUST DLL *****")
 child = subprocess.Popen(['python', './.github/workflows/store-dll.py'])
 child.wait()
-child = subprocess.Popen([UNITY_PATH, '-projectPath', '.', '-batchmode', '-buildTarget', TARGET, '-logFile' '-'])
+print("***** COMPILING UNITY *****")
+child = subprocess.Popen([UNITY_PATH, '-projectPath', '.', '-batchmode', '-buildTarget', TARGET, '-logFile' '-', '-quit', '-nographics', '-customBuildPath', build_path, '-executeMethod', 'CLIBuild.Builder.BuildProjectParse'])
 child.wait()
