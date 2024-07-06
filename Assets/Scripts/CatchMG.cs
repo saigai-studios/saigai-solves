3e241aabde50bada6c628576f1f4a8835416c6f3
Joseph Benigno <jbenigno@ufl.edu>
2024-07-06 16:47:53 +0900

    Randomizing falling objs pt 1

--- Assets/Scripts/CatchMG.cs
+++ Assets/Scripts/CatchMG.cs
@@ -4,7 +4,8 @@ using Saigai.Studios;
 [System.Serializable]
 public class CatchMG : MonoBehaviour
 {
-    public GameObject pet, rock, spawnPlane;
+    public GameObject dog, rock, spawnPlane;
+    //public GameObject[] objs = {, rock};
     public float spawnTime;
     public float spawnVar;
     public float spawnWidth = 2.0f;
@@ -24,9 +25,17 @@ public class CatchMG : MonoBehaviour
         {
             var newPos = spawnPlane.transform.position;
             newPos.z = newPos.z + Random.Range(-1.0f * spawnWidth, spawnWidth);
-            
+            int id = Random.Range(0, 2);
+
             // Spawn item
-            Instantiate(pet, newPos, Quaternion.Euler(0,90,0), this.transform);
+            GameObject obj = id switch
+            {
+                0 => dog,
+                1 => rock,
+                _ => dog,
+            };
+            Debug.Log(id);
+            Instantiate(obj, newPos, Quaternion.Euler(0,90,0), this.transform);
             resetCount();
         }
     }
