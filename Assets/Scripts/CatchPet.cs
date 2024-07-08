a01572c42f7a118938c8f44b67d5405dc3713487
Thomas Le <thomas.le@ufl.edu>
2024-07-08 12:38:26 +0900

    Added random sprite for pet

--- Assets/Scripts/CatchPet.cs
+++ Assets/Scripts/CatchPet.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CatchPet : MonoBehaviour
 {
+    public List<Sprite> sprList;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        // Pick random sprite
+        int randInd = Random.Range(0, sprList.Count);
+        Sprite newSpr = sprList[randInd];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Set as renderer
+        gameObject.GetComponent<Image>().sprite = newSpr;
     }
 }
